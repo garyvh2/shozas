bd75e1cb10f0cfde54ad76f8dd9972e911fe8bc5
Luis Daniel Morales Vargas <lmoralesv@ucenfotec.ac.cr>
2019-04-08 00:33:53 -0600

    Functional Pricing, Pending: Illustration.

---
+++ src/main/webapp/app/modules/pricing/views/pricing-view/pricing-view.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { Options } from 'ng5-slider';
+import { SearchFilter } from 'app/@akita/external-models/searchFilter';
+import { PricingService } from '../../pricing.service';
+import { ApiResponse } from 'app/@akita/external-models/apiResponse.model';
+
+@Component({
+    selector: 'jhi-pricing-view',
+    templateUrl: './pricing-view.component.html',
+    styleUrls: ['./pricing-view.component.scss']
+})
+export class PricingViewComponent implements OnInit {
+    priceRange: Options = {
+        ceil: 0,
+        floor: 0
+    };
+    sizeRange: Options = {
+        ceil: 0,
+        floor: 0
+    };
+
+    homeFilters: SearchFilter = new SearchFilter();
+
+    averagePrice = 0;
+
+    constructor(private pricingService: PricingService) {}
+
+    ngOnInit() {
+        this.homeFilters.sizeLow = 100;
+        this.homeFilters.baths = 1;
+        this.homeFilters.beds = 1;
+        this.homeFilters.garages = 1;
+        this.homeFilters.stories = 1;
+        this.homeFilters.similarTo = true;
+    }
+
+    applyFilters() {
+        console.log('SF: ', this.homeFilters);
+        this.pricingService.getPricing(this.homeFilters).subscribe(response => {
+            console.log(response);
+            this.averagePrice = response.result.averagePrice;
+        });
+    }
+}
